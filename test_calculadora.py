65a03982a6103b431cdacc0dd3cbab93f574c6a8
MJavoso <marcoapdr15@gmail.com>
2026-04-22 11:34:07 -0600

    Corrigiendo formato de flake8

--- test_calculadora.py
+++ test_calculadora.py
@@ -6,7 +6,7 @@ class TestCalculadora(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         print('setUpClass')
-    
+
     @classmethod
     def tearDownClass(cls):
         print('tearDownClass')
@@ -14,10 +14,10 @@ class TestCalculadora(unittest.TestCase):
     def setUp(self):
         self.calc = Calculadora()
         print('setUp')
-    
+
     def tearDown(self):
         print('tearDown')
-    
+
     def test_sumar_dos_mas_dos(self):
         # AAA
 
@@ -26,19 +26,19 @@ class TestCalculadora(unittest.TestCase):
         resultado = self.calc.sumar(2, 2)
         # Assert
         self.assertEqual(4, resultado)
-    
+
     def test_cinco_mas_tres(self):
         resultado = self.calc.sumar(5, 3)
         self.assertEqual(8, resultado)
-    
+
     def test_sumar_string_falla(self):
         resultado = self.calc.sumar('f', 2)
         self.assertEqual('Sólo se admiten números', resultado)
-    
+
     def test_sumar_string2_falla(self):
         resultado = self.calc.sumar(5, 'xd')
         self.assertEqual('Sólo se admiten números', resultado)
-    
+
     def test_sumar_negativos_falla(self):
         resultado = self.calc.sumar(-4, 6)
         self.assertEqual('Solo se admiten números positivos', resultado)
@@ -49,15 +49,22 @@ class TestCalculadora(unittest.TestCase):
 
     def test_sumar_decimales_falla(self):
         resultado = self.calc.sumar(4.5, 6)
-        self.assertEqual('Solo se admiten números enteros positivos', resultado)
-    
-    def test_sumar_decimales_falla(self):
+        self.assertEqual(
+            'Solo se admiten números enteros positivos',
+            resultado
+        )
+
+    def test_sumar_decimales_falla2(self):
         resultado = self.calc.sumar(4, 9.9)
-        self.assertEqual('Solo se admiten números enteros positivos', resultado)
-    
+        self.assertEqual(
+            'Solo se admiten números enteros positivos',
+            resultado
+        )
+
     def test_restar_ocho_menos_cinco(self):
         resultado = self.calc.restar(8, 5)
         self.assertEqual(3, resultado)
+
 
 if __name__ == '__main__':
     unittest.main()
